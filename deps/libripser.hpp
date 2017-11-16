b9b3b74565a9c6ec62dbd553f2be8887f5f0cd05
mtsch <matijacufar@gmail.com>
2017-11-16 10:27:38 +0100

    Fixed clang build.

--- deps/libripser.hpp
+++ deps/libripser.hpp
@@ -2,7 +2,6 @@
 extern "C" {
 #endif
 void ripser(index_t n, value_t *mat, index_t dim_max, value_t threshold, coefficient_t modulus);
-  int main(int argc, char **argv);
 #ifdef __cplusplus
 }
 #endif
